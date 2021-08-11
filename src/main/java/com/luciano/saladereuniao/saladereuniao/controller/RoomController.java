13c570631845cf7768e37240d704e873af77c8db
lufranca <lucyano_spfc@hotmail.com>
2021-08-10 23:31:56 -0300

    endpoint api/rooms disponivel, metodo geAllRooms

--- src/main/java/com/luciano/saladereuniao/saladereuniao/controller/RoomController.java
+++ src/main/java/com/luciano/saladereuniao/saladereuniao/controller/RoomController.java
@@ -1,7 +1,26 @@
 package com.luciano.saladereuniao.saladereuniao.controller;
 
-import org.springframework.stereotype.Controller;
+import com.luciano.saladereuniao.saladereuniao.model.Room;
+import com.luciano.saladereuniao.saladereuniao.repository.RoomRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import java.util.List;
+
+@CrossOrigin(origins = "http://locahost:4200")
+@RestController
+@RequestMapping(value = "/api")
 public class RoomController {
+
+    @Autowired
+    private RoomRepository roomRepository;
+
+    @GetMapping("/rooms")
+    public List<Room> getAllRooms() {
+        return roomRepository.findAll();
+    }
 }
